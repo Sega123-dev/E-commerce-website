7be83de894e7b3897bd56287f9d1601d1af2f6fa
Sega123-dev <brajicsergej@gmail.com>
2024-09-26 18:10:15 +0200

    Fixing of routes

--- src/server.js
+++ src/server.js
@@ -2,13 +2,26 @@
 const express = require("express");
 const app = express();
 app.use(express.static("client"));
+require("dotenv").config();
+const path = require("path");
 app.get("/login", (req, res) => {
-    res.send("Login");
+    res.sendFile(path.join(__dirname, "../client/login", "login.html"));
 });
 app.get("/men", (req, res) => {
-    res.send("Men");
+    res.sendFile(path.join(__dirname, "../client/men", "men.html"));
 });
 app.get("/women", (req, res) => {
-    res.send("Women");
+    res.sendFile(path.join(__dirname, "../client/women", "women.html"));
 });
 app.listen(3000);
+function countryFetch() {
+    fetch(`https://ipinfo.io/json?token=${process.env.IP_INFO_TOKEN}`).then((response) => response
+        .json()
+        .then((data) => {
+        console.log(data);
+    })
+        .catch((error) => {
+        console.error(error);
+    }));
+}
+countryFetch();
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,32 @@
 const express = require("express");
 const app = express();
 app.use(express.static("client"));
+require("dotenv").config();
+const path = require("path");
 
 app.get("/login", (req: any, res: any): void => {
-  res.send("Login");
+  res.sendFile(path.join(__dirname, "../client/login", "login.html"));
 });
 app.get("/men", (req: any, res: any): void => {
-  res.send("Men");
+  res.sendFile(path.join(__dirname, "../client/men", "men.html"));
 });
 app.get("/women", (req: any, res: any): void => {
-  res.send("Women");
+  res.sendFile(path.join(__dirname, "../client/women", "women.html"));
 });
+
 app.listen(3000);
+
+function countryFetch(): void {
+  fetch(`https://ipinfo.io/json?token=${process.env.IP_INFO_TOKEN}`).then(
+    (response) =>
+      response
+        .json()
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+  );
+}
+countryFetch();
